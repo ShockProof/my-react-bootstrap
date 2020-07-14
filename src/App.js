ef8068a1c297fdbdd3e44867ee086b5e2751c3b5
ShockProof <zafrullipu@gmail.com>
2020-07-14 20:03:04 +0600

    add ui component samples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CookiesProvider } from 'react-cookie';
 
-function App() {
+import ThemeProvider from './style-root/ThemeProvider';
+import App from './ui-v1/App';
+
+export default function Root() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CookiesProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </CookiesProvider>
   );
 }
-
-export default App;
---
+++ src/ui-v1/App.jsx
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react';
+
+import ThemeContext from './../style-root/ThemeContext';
+
+import { Container, Row, Col, Button, ButtonGroup, Form } from 'react-bootstrap';
+
+import SiteHeader from './../components/SiteHeader';
+
+function App() {
+
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <div>
+      <SiteHeader title="Site Header"/>
+
+      <Container>
+        <Row>
+          <Col>
+            <hr></hr>
+
+            <ButtonGroup size="sm" block>
+              <Button onClick={() => themeContext.setTheme('first')}>first</Button>
+              <Button onClick={() => themeContext.setTheme('second')}>second</Button>
+              <Button onClick={() => themeContext.setTheme('third')}>third</Button>
+            </ButtonGroup>
+
+            <hr></hr>
+            <Button size="sm" variant="primary">Primary</Button> {' '}
+            <Button variant="info">Info</Button> {' '}
+            <Button size="sm" variant="danger">Danger</Button>
+
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label size="sm">Email address</Form.Label>
+                <Form.Control size="sm" type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicTextarea">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit"> Submit </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;

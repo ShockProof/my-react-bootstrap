42394c9f3b10dcecb3113dfc2521d9c9b25529bc
ShockProof <zafrullipu@gmail.com>
2020-08-09 18:49:56 +0600

    replace <hr> with empty <div>

--- src/ui-v1/App.jsx
+++ src/ui-v1/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <Container>
         <Row>
           <Column>
-            <hr></hr>
+            <div style={{height: 16}}></div>
             <ThemePicker />
 
             <Card title="Card TItle">
